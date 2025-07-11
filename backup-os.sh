99bd2155bc42d1ca9f7c4b91c7b4927622305239
Evgeniy Buyanov <buyanov.evgeniy3@wb.ru>
2025-07-11 15:04:19 +0300

    backup

--- backup-os.sh
+++ backup-os.sh
@@ -20,5 +20,5 @@ mkdir -p "$BACKUP_DIR/$DATE"
 
 rsync -aAXv / "${EXCLUDES[@]}" "$BACKUP_DIR/$DATE" | tee "$BACKUP_DIR/backup-$DATE.log"
 
-# (Опционально) удалить бэкапы старше 7 дней
-find "$BACKUP_DIR" -maxdepth 1 -type d -mtime +7 -exec rm -rf {} +
+# (Опционально) удалить бэкапы старше 90 дней
+find "$BACKUP_DIR" -maxdepth 1 -type d -mtime +90 -exec rm -rf {} +
